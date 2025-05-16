5dc624fce7e85433b65a63047ec5a90b353c10f3
daniel73919@gmail.com <daniel73919@gmail.com>
2025-05-16 10:28:50 +0900

    update2nd

--- sideproject/sideproject4_electionsimulator/flutter_ui/build/web/flutter_service_worker.js
+++ sideproject/sideproject4_electionsimulator/flutter_ui/build/web/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "8a2ffc568818219be1f7108cdfe50a7d
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "e2c0430ae3706d7f0214e9c8ffa1e056",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "a70ee7379c6f4497cb2cfc981cc0b491",
+"flutter_bootstrap.js": "067f0d335ee0a982b32883c597dc8fe5",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
